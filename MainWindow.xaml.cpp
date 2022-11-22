fe5f88ec75272c94c042e6e921d7db4ff86e9685
comeng45 <20201110@student.anu.ac.kr>
2022-11-22 17:40:14 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::Team7::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+        this->Title(L"메뉴바 제작");
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+}
+
+
+
+void winrt::Team7::implementation::MainWindow::MenuFlyoutItem_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    Text1().Text(to_hstring("I love WinUI3"));
+}
